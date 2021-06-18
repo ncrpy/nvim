8245e22ee0ead580e8254db4c6b295f75bf79f02
ncrpy <61719652+ncrpy@users.noreply.github.com>
2021-06-18 21:27:53 +0900

    Add some treesitter plugins and disable lexima

--- plugins/lua/nvim-treesitter.lua
+++ plugins/lua/nvim-treesitter.lua
@@ -5,4 +5,9 @@ require'nvim-treesitter.configs'.setup {
         enable = true,
         disable = {},
     },
+  rainbow = {
+    enable = true,
+    extended_mode = true,
+    max_file_lines = 1000,
+  }
 }
