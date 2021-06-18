5147b723444ac70f04826136b8825ffa0bfb73c7
ncrpy <61719652+ncrpy@users.noreply.github.com>
2021-06-18 19:41:43 +0900

    Add: settings for formatter.nvim

--- plugins/lua/formatter.lua
+++ plugins/lua/formatter.lua
@@ -1,25 +1,80 @@
-require('formatter').setup({
-  loggin = false,
-  filetype = {
-    python = {
+local filetype = {}
+local ft_table = {"c", "cpp", "python"}
+local ft_supported = false
 
-      function()
-        return {
-          exe = vim.g.python3_host_prog.." -m isort",
-          args = {"-"},
-          stdin = true
-        }
-      end,
-      function()
-        return {
-          exe = vim.g.python3_host_prog.." -m black",
-          args = {"-"},
-          stdin = true
-        }
-      end
+local global_formatters = {}
 
-    }
+local function merge_table(tb1, tb2)
+  for _, v in pairs(tb2) do table.insert(tb1, v) end
+  return tb1
+end
+
+for _, ft in pairs(ft_table) do
+  filetype[ft] = merge_table({}, global_formatters)
+  if ft == vim.bo.filetype then ft_supported = true end
+end
+if not(ft_supported) then filetype[vim.bo.filetype] = merge_table({}, global_formatters) end
+
+--[[Template
+for _, ft in pairs({}) do
+  local formatters = {
+    function()
+      return {
+        exe = "",
+        args = {},
+        stdin = true,
+      }
+    end
   }
+  for __, func in pairs(formatters) do table.insert(filetype[ft], func) end
+  merge_table({}, formatters)
+end
+]]
+
+for _, ft in pairs({"c", "cpp"}) do
+  local formatters = {
+    function()
+      return {
+        exe = "clang-format",
+        args = {"-assume-filename="..vim.fn.bufname("%"), "-style='{IndentWidth: 4}'"},
+        stdin = true,
+      }
+    end
+  }
+  merge_table(filetype[ft], formatters)
+end
+
+for _, ft in pairs({"python"}) do
+  local formatters = {
+    function()
+      return {
+        exe = vim.g.python3_host_prog.." -m isort",
+        args = {"-"},
+        stdin = true,
+      }
+    end,
+    function()
+      return {
+        exe = vim.g.python3_host_prog.." -m black",
+        args = {"-"},
+        stdin = true,
+      }
+    end
+  }
+  merge_table(filetype[ft], formatters)
+end
+
+require('formatter').setup({
+  logging = false,
+  filetype = filetype
 })
 
 vim.api.nvim_set_keymap('n', '<leader>F', ':Format<CR>', { noremap=true, silent=true })
+if ft_supported then
+  vim.api.nvim_exec([[
+  augroup FormatAutogroup
+    autocmd!
+    autocmd BufWritePost * FormatWrite
+  augroup END
+  ]], true)
+end
