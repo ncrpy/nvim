52e777538cceca6fa1e9aaf20bb820841ff5c2cd
ncrpy <61719652+ncrpy@users.noreply.github.com>
2021-12-17 02:00:46 +0900

    Improve: using ts-rainbow

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -4,26 +4,26 @@ require "nvim-treesitter.configs".setup {
   highlight = {
     enable = true,
     disable = {"latex"}
+  },
+  rainbow = {
+    enable = true,
+    extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
+    max_file_lines = nil, -- Do not enable for files with more than 1000 lines, int
+    colors = {
+      "#f92672",
+      "#ef5939",
+      "#e6db74",
+      "#a6e22e",
+      "#66d9ef",
+      "#ae81ff",
+      "#7070f0"
+    }
   }
-  --  rainbow = {
-  --    enable = false,
-  --    extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
-  --    max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
-  --    colors = {
-  --      "#ef5939",
-  --      "#66d9ef",
-  --      "#f92672",
-  --      "#e6db74",
-  --      "#ae81ff",
-  --      "#fd971f",
-  --      "#a6e22e"
-  --    }
-  --  }
 }
 
 --  for non-ts rainbow plugin; I don't know why it works :P
-require "nvim-treesitter.highlight"
-local hlmap = vim.treesitter.highlighter.hl_map
-hlmap.error = nil
-hlmap["punctuation.delimiter"] = "Delimiter"
-hlmap["punctuation.bracket"] = nil
+-- require "nvim-treesitter.highlight"
+-- local hlmap = vim.treesitter.highlighter.hl_map
+-- hlmap.error = nil
+-- hlmap["punctuation.delimiter"] = "Delimiter"
+-- hlmap["punctuation.bracket"] = nil
