c3f85a0e399b1d03b08784fc30c3fd485a4a598c
ncrpy <61719652+ncrpy@users.noreply.github.com>
2022-04-24 22:29:10 +0900

    Improve: use lualine instead of airline

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,94 @@
+require("lualine").setup {
+  options = {
+    icons_enabled = true,
+    theme = "auto",
+    component_separators = {left = "", right = ""},
+    section_separators = {left = "", right = ""},
+    disabled_filetypes = {},
+    always_divide_middle = true,
+    globalstatus = true
+  },
+  sections = {
+    lualine_a = {"mode"},
+    lualine_b = {
+      { "b:gitsigns_head", icon = "" }
+    },
+    lualine_c = {
+      {
+        "diff",
+        colored = true, -- Displays a colored diff status if set to true
+        --  diff_color = {
+        --    added    = 'GitSignsAdd',
+        --    modified = 'GitSignsChange',
+        --    removed  = 'GitSignsDelete',
+        --  },
+        symbols = {added = " ", modified = " ", removed = " "}, -- Changes the symbols used by the diff.
+        source = function()
+          local gitsigns = vim.b.gitsigns_status_dict
+          if gitsigns then
+            return {
+              added = gitsigns.added,
+              modified = gitsigns.changed,
+              removed = gitsigns.removed
+            }
+          end
+        end
+      },
+      {
+        "diagnostics",
+        -- Table of diagnostic sources, available sources are:
+        --   'nvim_lsp', 'nvim_diagnostic', 'coc', 'ale', 'vim_lsp'.
+        -- or a function that returns a table as such:
+        --   { error=error_cnt, warn=warn_cnt, info=info_cnt, hint=hint_cnt }
+        sources = {"nvim_lsp"},
+
+        -- Displays diagnostics for the defined severity types
+        sections = {"error", "warn", "info", "hint"},
+
+        --  diagnostics_color = {
+        --    -- Same values as the general color option can be used here.
+        --    error = 'DiagnosticError', -- Changes diagnostics' error color.
+        --    warn  = 'DiagnosticWarn',  -- Changes diagnostics' warn color.
+        --    info  = 'DiagnosticInfo',  -- Changes diagnostics' info color.
+        --    hint  = 'DiagnosticHint',  -- Changes diagnostics' hint color.
+        --  },
+        symbols = {error = " ", warn = " ", info = " ", hint = " "},
+        colored = true,           -- Displays diagnostics status in color if set to true.
+        update_in_insert = false, -- Update diagnostics in insert mode.
+        always_visible = false,   -- Show diagnostics even if there are none.
+      },
+      "filename"
+    },
+    lualine_x = {
+      "encoding",
+      {
+        "fileformat",
+        icons_enabled = true,
+        symbols = {
+          unix = "LF",
+          dos = "CRLF",
+          mac = "CR",
+        }
+      }
+    },
+    lualine_y = {"filetype"},
+    lualine_z = {"progress", "location"},
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {"filename"},
+    lualine_x = {"location"},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {
+    lualine_a = {"buffers"},
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {"tabs"},
+  },
+  extensions = {"nvim-tree"}
+}
