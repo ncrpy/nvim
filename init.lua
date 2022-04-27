1a91d1985b2af124c2539ef82a4f99a75cfecc75
ncrpy <61719652+ncrpy@users.noreply.github.com>
2022-04-27 11:05:38 +0900

    Fix init.lua

--- init.lua
+++ init.lua
@@ -3,23 +3,23 @@ vim.g.python3_host_prog = vim.fn.stdpath("config") .. "/venv/bin/python"
 vim.api.nvim_create_user_command(
   "SourceCfg",
   function(opts)
-		local cfg = opts.args
-		local tbl = vim.fn.split(cfg, "\\.")
-		if tbl[#tbl] == "vim" then
+    local cfg = opts.args
+    local tbl = vim.fn.split(cfg, "\\.")
+    if tbl[#tbl] == "vim" then
       local cfg_path = vim.fn.stdpath("config") .. "/plugins/" .. cfg
-			if vim.fn.filereadable(cfg_path) then
-				vim.cmd("source " .. cfg_path)
-			end
-		elseif tbl[#tbl] == "lua" then
-			local cfg_path = vim.fn.stdpath("config") .. "/lua/plugins/" .. cfg
-			if vim.fn.filereadable(cfg_path) then
-				dofile(cfg_path)
-			end
-		elseif package.loaders[2](cfg) then
-			require("plugins." .. cfg)
-		end
-	end,
-	{ nargs = 1 }
+      if vim.fn.filereadable(cfg_path) == 1 then
+        vim.cmd("source " .. cfg_path)
+      end
+    elseif tbl[#tbl] == "lua" then
+      local cfg_path = vim.fn.stdpath("config") .. "/lua/plugins/" .. cfg
+      if vim.fn.filereadable(cfg_path) == 1 then
+        dofile(cfg_path)
+      end
+    elseif package.loaders[2](cfg) then
+      require("plugins." .. cfg)
+    end
+  end,
+  {nargs = 1}
 )
 
 vim.cmd([[
