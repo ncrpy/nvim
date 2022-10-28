af512a2a4782d12fe1dbe85ca2c21be95f553ce3
ncrpy <61719652+ncrpy@users.noreply.github.com>
2022-10-28 13:16:36 +0900

    Use command palette

---
+++ lua/plugins/telescope-map.lua
@@ -0,0 +1,18 @@
+vim.keymap.set('n', '<leader>f<Space>', ':Telescope<Space>')
+
+vim.keymap.set('n', '<leader>ff', '<Cmd>lua require("telescope.builtin").find_files()<CR>')
+vim.keymap.set('n', '<leader>fg', '<Cmd>lua require("telescope.builtin").live_grep()<CR>')
+vim.keymap.set('n', '<leader>fb', '<Cmd>lua require("telescope.builtin").buffers()<CR>')
+vim.keymap.set('n', '<leader>fo', '<Cmd>lua require("telescope.builtin").oldfiles()<CR>')
+vim.keymap.set('n', '<leader>fc', '<Cmd>lua require("telescope.builtin").commands()<CR>')
+vim.keymap.set('n', '<leader>fj', '<Cmd>lua require("telescope.builtin").jumplist()<CR>')
+vim.keymap.set('n', '<leader>fv', '<Cmd>lua require("telescope.builtin").registers()<CR>')
+vim.keymap.set('n', '<leader>fz', '<Cmd>lua require("telescope.builtin").current_buffer_fuzzy_find()<CR>')
+
+vim.keymap.set('n', '<leader>fr', '<Cmd>lua require("telescope.builtin").lsp_references()<CR>')
+vim.keymap.set('n', '<leader>fs', '<Cmd>lua require("telescope.builtin").lsp_document_symbol()<CR>')
+vim.keymap.set('n', '<leader>fd', '<Cmd>lua require("telescope.builtin").diagnostic()<CR>')
+
+vim.keymap.set('n', '<leader>fb', '<Cmd>lua require("telescope.builtin").builtin()<CR>')
+
+vim.keymap.set('n', '<leader>c', '<Cmd>lua require("telescope").extensions.command_palette.command_palette()<CR>')
