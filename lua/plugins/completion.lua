0821636d6ee5317ae25c2317a1590798f63fc62d
ncrpy <61719652+ncrpy@users.noreply.github.com>
2023-04-12 16:34:51 +0900

    Add completion settings

---
+++ lua/plugins/completion.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      { "hrsh7th/cmp-buffer" },
+      { "hrsh7th/cmp-path" },
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "hrsh7th/cmp-nvim-lua" },
+      { "saadparwaiz1/cmp_luasnip", dependencies = "L3MON4D3/LuaSnip" }
+    },
+    event = { "InsertEnter" },
+    config = function()
+      require("plugins.config.cmp")
+    end,
+  },
+
+  {
+    "L3MON4D3/LuaSnip",
+    version = "<CurrentMajor>.*", -- follow latest release.
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+    },
+    config = true
+  },
+
+  {
+    "windwp/nvim-autopairs",
+    dependencies = {
+      "hrsh7th/nvim-cmp"
+    },
+    event = { "InsertEnter" },
+    config = function()
+      require("plugins.config.nvim-autopairs")
+    end
+  },
+}
