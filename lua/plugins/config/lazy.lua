63723f50ac682c9f90e26ca074edc1fdfecd30d2
ncrpy <61719652+ncrpy@users.noreply.github.com>
2023-06-05 15:11:32 +0900

    remove lazy icon settings -- use newer version of Nerd Fonts

--- lua/plugins/config/lazy.lua
+++ lua/plugins/config/lazy.lua
@@ -3,21 +3,4 @@ return {
     lazy = true,
     version = "*"
   },
-  ui = {
-    icons = {
-      lazy = "鈴",
-      cmd = "גּ",
-      config = "",
-      event = "",
-      ft = "",
-      init = "",
-      import = "",
-      keys = "",
-      plugin = "",
-      runtime = "",
-      source = "",
-      start = "ﰇ",
-      task = ""
-    }
-  }
 }
