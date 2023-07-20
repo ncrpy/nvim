126ae94b16aebbc8aeed130ea63a00d7d0275775
ncrpy <61719652+ncrpy@users.noreply.github.com>
2023-07-20 14:47:12 +0900

    Rewrite options.lua to Lua

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,84 +1,90 @@
-vim.cmd([[
-syntax on
+local opt = vim.opt
+local noremap = vim.keymap.set
 
-set hidden
-set autoread
+opt.syntax = "on"
 
-set encoding=utf-8
-set ambiwidth=single
+opt.hidden = true
+opt.autoread = true
 
-set number
-set ruler
+opt.encoding = "utf-8"
+opt.ambiwidth = "single"
 
-set laststatus=3
-set showcmd
-set showmode
+opt.number = true
+opt.ruler = true
 
-set showmatch
-set matchtime=1
+opt.laststatus = 3
+opt.showcmd = true
+opt.showmode = true
 
-set splitbelow
-set splitright
+opt.showmatch = true
+opt.matchtime = 1
 
-set nowildmenu
-set wildmode=list:longest,full
-set completeopt=menuone,noselect
+opt.splitbelow = true
+opt.splitright = true
 
-set backspace=indent,eol,start
-set whichwrap=b,s,h,l,<,>,[,],~
-set virtualedit=onemore,block
+opt.wildmenu = false
+opt.wildmode = { "list:longest", "full" }
+opt.completeopt = { "menuone", "noselect" }
 
-set clipboard&
-set clipboard^=unnamedplus
+opt.backspace = { "indent", "eol", "start" }
+opt.whichwrap:append "h,l,<,>,[,],~"
+opt.virtualedit = { "onemore", "block" }
 
-set hlsearch
-set incsearch
-set ignorecase
-set smartcase
+opt.clipboard:prepend { "unnamed", "unnamedplus" }
 
-set expandtab
-set tabstop=2
-set softtabstop=0
-set shiftwidth=0
-set autoindent
-set smartindent
+opt.hlsearch = true
+opt.incsearch = true
+opt.ignorecase = true
+opt.smartcase = true
 
-set nofixeol
+opt.expandtab = true
+opt.tabstop = 2
+opt.softtabstop = 0
+opt.shiftwidth = 0
+opt.autoindent = true
+opt.smartindent = true
 
-set list
-set listchars=tab:»-,trail:-,nbsp:%,eol:↲
+opt.fixeol = false
 
-if executable("rg")
-  set grepprg=rg\ --vimgrep\ --no-heading\ --smart-case
-  set grepformat+=%f:%l:%c:%m
-endif
+opt.list = true
+opt.listchars = {
+  tab = "»-",
+  trail = "-",
+  nbsp = "%",
+  eol = "↲"
+}
 
-noremap <C-l> :<C-u>nohlsearch<CR><C-l>
-noremap <leader>w <C-w>
+if vim.fn.executable("rg") then
+  opt.grepprg = "rg --vimgrep --no-heading --smart-case"
+  opt.grepformat:prepend { "%f:%l:%c:%m" }
+end
 
-noremap <silent> <leader>ba :<C-u>enew<CR>
-noremap <silent> <leader>bb :<C-u>b#<CR>
-noremap <silent> <leader>bm :<C-u>bmodified<CR>
-noremap <silent> <leader>bn :<C-u>bnext<CR>
-noremap <silent> <leader>bp :<C-u>bprevious<CR>
-noremap <silent> <leader>bf :<C-u>bfirst<CR>
-noremap <silent> <leader>bl :<C-u>blast<CR>
-noremap <silent> <leader>bc :<C-u>bdelete<CR>
-noremap <silent> <leader>bd :<C-u>bdelete<CR>
-noremap <silent> <leader>bD :<C-u>bdelete!<CR>
-noremap <silent> <leader>bo :<C-u>%bdelete<Bar>edit#<Bar>bdelete#<CR>
+noremap("", "<C-l>", ":<C-u>nohlsearch<CR><C-l>")
+noremap("", "<leader>w", "<C-w>")
 
-noremap <silent> <leader>i :<C-u>set invrelativenumber<CR>
+noremap("", "<leader>ba", "<Cmd>enew<CR>")
+noremap("", "<leader>bb", "<Cmd>b#<CR>")
+noremap("", "<leader>bm", "<Cmd>bmodified<CR>")
+noremap("", "<leader>bn", "<Cmd>bnext<CR>")
+noremap("", "<leader>bp", "<Cmd>bprevious<CR>")
+noremap("", "<leader>bf", "<Cmd>bfirst<CR>")
+noremap("", "<leader>bl", "<Cmd>blast<CR>")
+noremap("", "<leader>bc", "<Cmd>bdelete<CR>")
+noremap("", "<leader>bd", "<Cmd>bdelete<CR>")
+noremap("", "<leader>bD", "<Cmd>bdelete!<CR>")
+noremap("", "<leader>bo", "<Cmd>%bdelete<Bar>edit#<Bar>bdelete#<CR>")
+noremap("", "<leader>i", function()
+  opt.relativenumber = not opt.relativenumber
+end)
 
-noremap <silent> <leader>a ggVG
+noremap("", "<leader>a", "ggVG")
 
-noremap ; :
-noremap ,. ;
-noremap ,, ,
+noremap("", ";", ":")
+noremap("", ",.", ";")
+noremap("", ",,", ",")
 
-inoremap <C-l> <C-g>U<Right>
+noremap("i", "<C-l>", "<C-g>U<Right>")
 
-nnoremap <leader>s :<C-u>%s///g<Left><Left>
+noremap("n", "<leader>s", ":<C-u>%s///g<Left><Left>")
 
-tnoremap <silent> <ESC> <ESC><C-\><C-n>
-]])
+noremap("t", "<Esc>", "<Esc><C-\\><C-n>")
