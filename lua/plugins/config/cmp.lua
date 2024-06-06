3deace0629a657e6b6f727ebebe936820614d14a
ncrpy <61719652+ncrpy@users.noreply.github.com>
2024-06-07 00:46:27 +0900

    Improve cmp

--- lua/plugins/config/cmp.lua
+++ lua/plugins/config/cmp.lua
@@ -6,11 +6,11 @@ M.opts = function()
   local lspkind = require("lspkind")
   local cmp = require("cmp")
 
-  local has_words_before = function()
-    unpack = unpack or table.unpack
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-  end
+  -- local has_words_before = function()
+  --   unpack = unpack or table.unpack
+  --   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  --   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  -- end
 
   return {
     snippet = {
@@ -30,7 +30,6 @@ M.opts = function()
       ["<C-b>"] = cmp.mapping.scroll_docs(-4),
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
       ["<C-s>"] = cmp.mapping.complete(),
-      ["<C-y>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
 
       ["<CR>"] = cmp.mapping({
         i = function(fallback)
@@ -43,6 +42,16 @@ M.opts = function()
         s = cmp.mapping.confirm({ select = true }),
         c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
       }),
+
+      ["<C-y>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })
+        elseif copilot_ok and suggestion.is_visible() then
+          suggestion.accept()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
 
       ["<C-e>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
@@ -77,14 +86,14 @@ M.opts = function()
       ["<Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
-        -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-        -- they way you will only jump inside the snippet region
-        elseif luasnip.expand_or_locally_jumpable() then
-          luasnip.expand_or_jump()
         elseif copilot_ok and suggestion.is_visible() then
           suggestion.accept()
-        elseif has_words_before() then
-          cmp.complete()
+        elseif luasnip.expand_or_locally_jumpable() then
+          -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+          -- they way you will only jump inside the snippet region
+          luasnip.expand_or_jump()
+        -- elseif has_words_before() then
+        --   cmp.complete()
         else
           fallback()
         end
@@ -94,22 +103,6 @@ M.opts = function()
         if cmp.visible() then
           cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
         elseif luasnip.jumpable(-1) then
-          luasnip.jump(-1)
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
-
-      ["<C-k>"] = cmp.mapping(function(fallback)
-        if luasnip.expand_or_locally_jumpable() then
-          luasnip.expand_or_jump()
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
-
-      ["<C-S-k>"] = cmp.mapping(function(fallback)
-        if luasnip.jumpable(-1) then
           luasnip.jump(-1)
         else
           fallback()
@@ -165,6 +158,9 @@ M.setup = function(_, opts)
   cmp.event:on("menu_closed", function()
     vim.b.copilot_suggestion_hidden = false
   end)
+
+  vim.api.nvim_create_user_command("CmpEnable", function() require("cmp").setup.buffer({enabled = true}) end, {})
+  vim.api.nvim_create_user_command("CmpDisable", function() require("cmp").setup.buffer({enabled = false}) end, {})
 end
 
 return M
