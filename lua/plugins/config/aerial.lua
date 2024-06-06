71aceff6b34e2b7d3af98faa105ad8a5e66e0a5d
ncrpy <61719652+ncrpy@users.noreply.github.com>
2024-06-06 18:43:55 +0900

    Change table.unpack to unpack but disabled lualine aerial extension

--- lua/plugins/config/aerial.lua
+++ lua/plugins/config/aerial.lua
@@ -36,45 +36,45 @@ M.setup = function(_, opts)
     end,
   })
 
-  local lualine_ok, lualine = pcall(require, "lualine")
-  if lualine_ok then
-    lualine.setup({
-      extensions = {
-        {
-          sections = {
-            lualine_a = { "filetype" },
-
-            -- Or you can customize it
-            lualine_c = {
-              {
-                "aerial",
-                -- The separator to be used to separate symbols in status line.
-                sep = " ) ",
-
-                -- The number of symbols to render top-down. In order to render only 'N' last
-                -- symbols, negative numbers may be supplied. For instance, 'depth = -1' can
-                -- be used in order to render only current symbol.
-                depth = nil,
-
-                -- When 'dense' mode is on, icons are not rendered near their symbols. Only
-                -- a single icon that represents the kind of current symbol is rendered at
-                -- the beginning of status line.
-                dense = false,
-
-                -- The separator to be used to separate symbols in dense mode.
-                dense_sep = ".",
-
-                -- Color the symbol icons.
-                colored = true,
-              },
-            },
-          },
-          filetypes = { "aerial" },
-        },
-        table.unpack(require("lualine").get_config().extensions),
-      },
-    })
-  end
+  -- local lualine_ok, lualine = pcall(require, "lualine")
+  -- if lualine_ok then
+  --   lualine.setup({
+  --     extensions = {
+  --       {
+  --         sections = {
+  --           lualine_a = { "filetype" },
+  --
+  --           -- Or you can customize it
+  --           lualine_c = {
+  --             {
+  --               "aerial",
+  --               -- The separator to be used to separate symbols in status line.
+  --               sep = " ) ",
+  --
+  --               -- The number of symbols to render top-down. In order to render only 'N' last
+  --               -- symbols, negative numbers may be supplied. For instance, 'depth = -1' can
+  --               -- be used in order to render only current symbol.
+  --               depth = nil,
+  --
+  --               -- When 'dense' mode is on, icons are not rendered near their symbols. Only
+  --               -- a single icon that represents the kind of current symbol is rendered at
+  --               -- the beginning of status line.
+  --               dense = false,
+  --
+  --               -- The separator to be used to separate symbols in dense mode.
+  --               dense_sep = ".",
+  --
+  --               -- Color the symbol icons.
+  --               colored = true,
+  --             },
+  --           },
+  --         },
+  --         filetypes = { "aerial" },
+  --       },
+  --       unpack(require("lualine").get_config().extensions),
+  --     },
+  --   })
+  -- end
 end
 
 return M
