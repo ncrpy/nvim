b9883a3afbd22f6a1fef209f30ecc0aade2d0d28
Takuya Kasamura <kasamura@mtl.t.u-tokyo.ac.jp>
2026-05-21 05:13:04 +0900

    Improved barbar check

--- lua/plugins/config/barbar.lua
+++ lua/plugins/config/barbar.lua
@@ -9,18 +9,25 @@ M.opts = {
 M.setup = function(_, opts)
   local group = vim.api.nvim_create_augroup("count_buffers", {})
   local barbar_setup = function()
-    if #(vim.fn.getbufinfo({ buflisted = 1 })) > 1 then
-      require("barbar").setup(opts)
-      vim.api.nvim_del_augroup_by_id(group)
+    local count = 0
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+      if vim.api.nvim_get_option_value("buflisted", { buf = buf }) then
+        count = count + 1
+        if count > 1 then
+          require("barbar").setup(opts)
+          vim.api.nvim_del_augroup_by_id(group)
+          return true
+        end
+      end
     end
   end
 
-  vim.api.nvim_create_autocmd("BufEnter", {
+  vim.api.nvim_create_autocmd("BufAdd", {
     group = group,
     callback = barbar_setup,
   })
 
-  barbar_setup()
+  vim.api.nvim_exec_autocmds("BufAdd", { group = group })
 end
 
 return M
